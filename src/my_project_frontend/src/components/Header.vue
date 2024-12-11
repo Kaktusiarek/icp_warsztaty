11cc07ea2e2c2f3326d68c062e31dc2766cfbe8a
Kacper Urban <kaktusiarek@gmail.com>
2024-12-11 13:20:20 +0000

    feat: calculator

---
+++ src/my_project_frontend/src/components/Header.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref } from 'vue';
+import { my_project_backend } from 'declarations/my_project_backend/index';
+let greeting = ref('');
+
+async function handleSubmit(e) {
+  e.preventDefault();
+  const target = e.target;
+  const name = target.querySelector('#name').value;
+  await my_project_backend.greet(name).then((response) => {
+    greeting.value = response;
+  });
+}
+</script>
+<template>
+<header>
+  <main>
+    <img src="/logo2.svg" alt="DFINITY logo" />
+    <br />
+    <br />
+    <form action="#" @submit="handleSubmit">
+      <label for="name">Enter your name: &nbsp;</label>
+      <input id="name" alt="Name" type="text" />
+      <button type="submit">Click Me!</button>
+    </form>
+    <section id="greeting">{{ greeting }}</section>
+  </main>
+</header>
+</template>
+
+<script>
+export default {
+    name: "Header",
+};
+</script>
